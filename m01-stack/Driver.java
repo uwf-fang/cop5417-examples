3f60a3edf1a0358ba4d7c1754e3e70a2d42463e8
Ian Fang <xfang@uwf.edu>
2023-06-21 14:17:42 -0500

    add stack example

---
+++ m01-stack/Driver.java
@@ -0,0 +1,9 @@
+
+public class Driver {
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        stack.push("Hello");
+        System.out.println("Value popped: " + stack.pop());
+    }
+
+}
