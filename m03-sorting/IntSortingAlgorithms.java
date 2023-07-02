f0057e8671037ae882654c20aab7f2b310ce18cd
Ian Fang <xfang@uwf.edu>
2023-07-02 14:04:13 -0500

    update module 3

--- m03-sorting/IntSortingAlgorithms.java
+++ m03-sorting/IntSortingAlgorithms.java
@@ -1,114 +1,141 @@
 public class IntSortingAlgorithms {
 
-  public static void insertionSort(int[] arr) {
-    int n = arr.length;
-    for (int i = 1; i < n; i++) {
-      int key = arr[i];
-      int j = i - 1;
-      while (j >= 0 && arr[j] > key) {
-        arr[j + 1] = arr[j];
-        j--;
-      }
-      arr[j + 1] = key;
-    }
-  }
-
-  public static void selectionSort(int[] arr) {
-    int n = arr.length;
-    for (int i = 0; i < n - 1; i++) {
-      int minIndex = i;
-      for (int j = i + 1; j < n; j++) {
-        if (arr[j] < arr[minIndex]) {
-          minIndex = j;
+    /**
+     * Insertion sort algorithm
+     */
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
         }
-      }
-      int temp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = temp;
-    }
-  }
-
-  public static void mergeSort(int[] arr) {
-    mergeSort(arr, 0, arr.length - 1);
-  }
-
-  private static void mergeSort(int[] arr, int low, int high) {
-    if (low < high) {
-      int mid = (low + high) / 2;
-      mergeSort(arr, low, mid);
-      mergeSort(arr, mid + 1, high);
-      merge(arr, low, mid, high);
-    }
-  }
-
-  private static void merge(int[] arr, int low, int mid, int high) {
-    int n1 = mid - low + 1;
-    int n2 = high - mid;
-
-    int[] left = new int[n1];
-    int[] right = new int[n2];
-
-    for (int i = 0; i < n1; i++) {
-      left[i] = arr[low + i];
-    }
-    for (int j = 0; j < n2; j++) {
-      right[j] = arr[mid + 1 + j];
     }
 
-    int i = 0, j = 0;
-    int k = low;
-
-    while (i < n1 && j < n2) {
-      if (left[i] <= right[j]) {
-        arr[k] = left[i];
-        i++;
-      } else {
-        arr[k] = right[j];
-        j++;
-      }
-      k++;
+    /**
+     * Selection sort algorithm
+     */
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 
-    while (i < n1) {
-      arr[k] = left[i];
-      i++;
-      k++;
+    /**
+     * Merge sort algorithm
+     */
+    public static void mergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
     }
 
-    while (j < n2) {
-      arr[k] = right[j];
-      j++;
-      k++;
+    /**
+     * Merge sort helper method to recursively sort the array
+     */
+    private static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
     }
-  }
 
-  public static void quickSort(int[] arr) {
-    quickSort(arr, 0, arr.length - 1);
-  }
+    /**
+     * Merge sort helper method to merge two sorted arrays
+     */
+    private static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
 
-  private static void quickSort(int[] arr, int low, int high) {
-    if (low < high) {
-      int pivotIndex = partition(arr, low, high);
-      quickSort(arr, low, pivotIndex - 1);
-      quickSort(arr, pivotIndex + 1, high);
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[low + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = low;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
     }
-  }
 
-  private static int partition(int[] arr, int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-      if (arr[j] < pivot) {
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
+    /**
+     * Quick sort algorithm
+     */
+    public static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return i + 1;
-  }
+
+    /**
+     * Quick sort helper method to recursively sort the array
+     */
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    /**
+     * Quick sort helper method to partition the array
+     * and return the pivot index
+     * Lumuto's partition scheme
+     * last element as pivot
+     */
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
 
 }
